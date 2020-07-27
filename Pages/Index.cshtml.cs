1d0b94cd70087030c01578b4007e8cbba1f03468
Jack Lichwa <jalichwa@microsoft.com>
2020-07-27 14:53:00 -0700

    Add web app

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+namespace KeyVault_Rotation_SQLPassword_Csharp_WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private IMemoryCache _cache;
+        private readonly IConfiguration config;
+        private const int numberOfRetries = 5;
+       
+        private string secretName= "";
+        private string dataSource = "";
+        private string keyVaultName = "";
+        
+
+        public string CredentialIdTag { get; set; }
+        public string SecretValue { get; set; }
+        public bool DatabaseConnected { get; set; }
+
+        public int RetriesCount { get; set; }
+
+        public IndexModel(IMemoryCache cache, IConfiguration config)
+        {
+            _cache = cache;
+            this.config = config;
+            if (!String.IsNullOrEmpty(config["DataSource"]))
+            {
+                dataSource = config["DataSource"];
+            }
+            if (!String.IsNullOrEmpty(config["KeyVaultName"]))
+            {
+                keyVaultName = config["KeyVaultName"];
+            }
+            if (!String.IsNullOrEmpty(config["SecretName"]))
+            {
+                secretName = config["SecretName"];
+            }
+        }
+        public void OnGet()
+        {
+            KeyVaultSecret secret = RetrieveSecret(false);
+
+            //DB Connection builder
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = dataSource;
+
+            for (int i = 0; i < numberOfRetries; i++)
+            {
+                builder.UserID = secret.Properties.Tags.ContainsKey("CredentialId")? secret.Properties.Tags["CredentialId"]:"";
+                builder.Password = secret.Value;
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                    {
+                        connection.Open();
+                        DatabaseConnected = true;
+                        
+                    }
+                    SqlConnection.ClearAllPools();
+                    break;
+                }
+                catch
+                {
+                    RetriesCount++;
+                    Thread.Sleep(500);
+
+                    //refresh secret
+                    secret= RetrieveSecret(true);
+
+                }
+            }
+        }
+
+        private KeyVaultSecret RetrieveSecret(bool ignoreCache)
+        {
+            KeyVaultSecret secret = null;
+            if (ignoreCache || !_cache.TryGetValue(secretName, out secret))
+            {
+                // Authenticate to Key Vault
+                var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+                var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+
+                //Retrieve Secret
+                secret = client.GetSecret(secretName);
+
+                //update cache
+                _cache.Set(secretName, secret, TimeSpan.FromHours(8));
+            }
+
+            CredentialIdTag = secret.Properties.Tags.ContainsKey("CredentialId") ? secret.Properties.Tags["CredentialId"] : "";
+            SecretValue = secret.Value;
+            return secret;
+        }
+    }
+}
---
+++ arm-templates/Readme.md
@@ -0,0 +1,11 @@
+# SQL Password Rotation Test Web App
+
+This template creates Web App with access to SQL Server and Key Vault to help demonstrate Azure SQL password rotation in Key Vault using Function and Event Grid notification.
+
+If you are new to the template development, see:
+
+- [Azure Resource Manager documentation](https://docs.microsoft.com/en-us/azure/azure-resource-manager/)
+- [Use Azure Key Vault to pass secure parameter value during deployment](https://docs.microsoft.com/azure/azure-resource-manager/resource-manager-keyvault-parameter)
+- [Tutorial: Integrate Azure Key Vault in Resource Manager Template deployment](https://docs.microsoft.com/azure/azure-resource-manager/resource-manager-tutorial-use-key-vault)
+
+Tags: Azure Key Vault, Key Vault, Secrets,Storage Account, Resource Manager, Resource Manager templates, ARM templates
